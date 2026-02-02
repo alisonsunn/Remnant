3a59072261444effeb403891b019ba302849215a
alisonsunn <100303274+alisonsunn@users.noreply.github.com>
2026-02-02 13:23:26 +1100

    Feature/update logout (#19)
    * update-logout-function
    
    * upload backend nodejs file
    
    ---------
    
    Co-authored-by: Alison Sun <alisonsun@Alisons-MacBook-Pro.local>

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,8 +1,25 @@
 "use client";
-
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+async function handleLogout(router: any) {
+  try {
+    const res = await fetch("http://localhost:3001/auth/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to logout");
+    }
+    router.replace("/");
+  } catch (error) {
+    console.error("Failed to logout:", error);
+  }
+  alert("Logged out successfully");
+}
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <nav className="w-full px-6 py-7 flex items-center justify-between mb-15.5">
       <Link href="/" className="text-xl font-light tracking-[0.8em] italic">
@@ -14,7 +31,7 @@ export default function Navbar() {
         <Link href="/record">Record</Link>
         <Link href="/moments">Moments</Link>
         <Link href="/signup">Vault</Link>
-        <Link href="/logout">Logout</Link>
+        <button onClick={() => handleLogout(router)}>Logout</button>
       </div>
     </nav>
   );
